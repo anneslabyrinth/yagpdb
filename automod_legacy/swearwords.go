9b456892ffef22e87e51c76d10806fb29f385c13
anneslabyrinth <94088332+anneslabyrinth@users.noreply.github.com>
2021-11-10 18:54:27 -0600

    Update swearwords.go

--- automod_legacy/swearwords.go
+++ automod_legacy/swearwords.go
@@ -1,76 +1,132 @@
-package automod_legacy
-
-// These words are listed in in alphabethical order.
-var BuiltinSwearWords = map[string]bool{
-	"anal":        true,
-	"anus":        true,
-	"arse":        true,
-	"ass":         true,
-	"b1tch":       true,
-	"ballsack":    true,
-	"bastard":     true,
-	"bitch":       true,
-	"biatch":      true,
-	"blowjob":     true,
-	"bollock":     true,
-	"bollok":      true,
-	"boner":       true,
-	"boob":        true,
-	"boobs":       true,
-	"buttplug":    true,
-	"clitoris":    true,
-	"cock":        true,
-	"cum":         true,
-	"cunt":        true,
-	"dick":        true,
-	"dildo":       true,
-	"dyke":        true,
-	"erection":    true,
-	"fag":         true,
-	"faggot":      true,
-	"feck":        true,
-	"fellate":     true,
-	"fellatio":    true,
-	"felching":    true,
-	"fuck":        true,
-	"fucks":       true,
-	"fudgepacker": true,
-	"genitals":    true,
-	"hell":        true,
-	"jerk":        true,
-	"jizz":        true,
-	"knobend":     true,
-	"labia":       true,
-	"masturbate":  true,
-	"muff":        true,
-	"nigger":      true,
-	"nigga":       true,
-	"penis":       true,
-	"piss":        true,
-	"poop":        true,
-	"pube":        true,
-	"pussy":       true,
-	"scrotum":     true,
-	"sex":         true,
-	"shit":        true,
-	"sh1t":        true,
-	"slut":        true,
-	"smegma":      true,
-	"spunk":       true,
-	"tit":         true,
-	"tranny":      true,
-	"trannies":    true,
-	"tosser":      true,
-	"turd":        true,
-	"twat":        true,
-	"vagina":      true,
-	"wank":        true,
-	"whore":       true,
-	"tits":        true,
-	"titty":       true,
-	"asshole":     true,
-	"fvck":        true,
-	"asshat":      true,
-	"pu55y":       true,
-	"pen1s":       true,
-}
+anal
+coochie
+dick
+bj
+blowjob
+coon 
+carpetmuncher
+chink
+clit
+clitorus
+cock
+cocksucker
+cuck
+cum 
+cunt
+fag
+F@g
+faggot
+faggots
+fap
+fapping
+faping
+faped
+fisting
+foreskin
+negor
+nig
+nigg
+nigga
+niggas
+nigger
+nihger
+nihgers
+niggers
+nigglet
+niggor
+niggress
+niggrows
+niggy
+penis
+pu$$y
+pussies
+pussy
+slut
+titties
+suicide 
+kys
+killyourself
+endyourlife
+Tranny
+thot
+gringo
+gringos
+xvideos 
+xvide0S 
+blowjob
+boner
+clit
+dildo
+ejaculat
+ejakulat
+ejakulation
+ejakulationen
+ejakulieren 
+fellatio
+cuck
+cuk
+cuckold
+cukold
+ffag
+gangbang
+gangrape
+gaywad
+honkey
+kike
+jiz
+jizz 
+masterbat
+masturbate
+masturbation
+masturbiere
+masturbieren
+masturbierst
+masturbiert
+orgie
+penisse
+pussy
+raghead
+rimjob
+sandnig
+sperm 
+sphinct
+sphincter
+spick
+spooge
+killurself
+killyourself
+beaner
+spearchucker
+spearchuckers
+nigge.r
+sieg heil
+redtube
+porn
+pornhub
+p0rnhub
+downsyndrome
+retard
+retarded
+incel
+incels
+orgy
+orgies
+rape
+whore
+pussy?
+cunt?
+cock?
+vagina
+dick?
+vagina?
+slut
+loli
+loli?
+loli!
+raping
+raped
+rape
+slut
+slut?
+rape?
+retards
